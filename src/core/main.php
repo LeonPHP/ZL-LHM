c8872951dd248f0384e8a476085dd5c72f560732
Leon Hazard Majestic <GamerCodeXzavier@gmail.com>
2017-03-24 17:49:49 +0800

    first commit

---
+++ src/core/main.php
@@ -0,0 +1,23 @@
+<php
+
+namespace core;
+
+class main{
+ 
+ protected function getCore($id, $key, $pass){
+  if($id === $this->getID(); $key ==== $this->getKEY(); $this->getData()->filePass()){
+  $core = array["","",""];
+  return $echo;
+  }else{
+  return false;
+  }
+ 
+ 
+ }
+ 
+ 
+ 
+ 
+}
+
+?>
